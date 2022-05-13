b6e182e3665f2bb6a67e1e3b3e5fea30811e27d4
uekodyn <jansantosd@gmail.com>
2022-05-13 12:49:33 -0300

    customizing login screen

--- src/App.vue
+++ src/App.vue
@@ -14,24 +14,32 @@
                <img src="./assets/perfillogo.svg" alt="" class="lg:hidden h-20 w-full">
                <H2 class="mt-6 text-3xl font-semibold text-blue-400">Entrar</H2>
                <p class="mt-2 text-sm text-gray-600 max-w">Novo por aqui? 
-                  <ModalCriarUsuario />
+                  <ModalCriarUsuario  class=""/>
                </p>
             </div>
+            <!-- form -->
             <div class="mt-6">
                <form action="">
                   <div class="mb-4">
                      <input type="text" placeholder="E-mail"
-                        class="apperance-none block w-full py-3 px-4 leading-tight text-gray-700 bg-gray-50 focus:bg-white border border-gray-200 focus:border-gray-500 rounded focus:outline-none">
+                        class="apperance-none shadow-md block w-full py-3 px-4 leading-tight text-gray-700 bg-gray-50 focus:bg-white border border-gray-200 focus:border-gray-500 rounded focus:outline-none">
                   </div>
                   <div class="mb-4">
-                     <input type="text" placeholder="Senha"
-                        class="apperance-none block w-full py-3 px-4 leading-tight text-gray-700 bg-gray-50 focus:bg-white border border-gray-200 focus:border-gray-500 rounded focus:outline-none">
+                     <input type="password" value="" placeholder="Senha"
+                        class="apperance-none shadow-md block w-full py-3 px-4 leading-tight text-gray-700 bg-gray-50 focus:bg-white border border-gray-200 focus:border-gray-500 rounded focus:outline-none">
+                  </div>
+                  <div class="flex justify-between mx-1">
+                     <label class="block text-gray-500 font-bold my-4">
+                        <input 
+                           type="checkbox"
+                           class="leading-loose text-blue-600">
+                           <span class="py-2 text-sm text-gray-600 leading-snug"> Remember Me </span></label> <label class="block text-gray-500 font-bold my-4"><a
+                              href="#"
+                              class="cursor-pointer tracking-tighter text-black border-b-2 border-gray-200 hover:border-gray-400"><span>Esqueceu a senha?</span></a></label>
                   </div>
                   <div class="mb-4">
-                     <button
-                        class="inline-block w-full py-4 px-8 leading-none text-white bg-blue-400 hover:bg-blue-900 font-semibold rounded shadow">Entrar</button>
+                     <button class="inline-block w-full py-4 px-8 leading-none text-white bg-blue-400 hover:bg-blue-500 font-semibold rounded shadow">Entrar</button>
                   </div>
-
                </form>
             </div>
          </div>
